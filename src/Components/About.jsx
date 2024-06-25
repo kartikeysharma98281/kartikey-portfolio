9325b813979737e12e760b10694e56ccfab576c7
kartikeysharma98281 <kartikeysharma.sharma@gmail.com>
2024-06-25 19:45:18 +0530

    new projects added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,50 +1,29 @@
-/**
- * About component
- *
- * Space for you to describe more about yourself.
- */
+
 
 import React from "react";
 
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
 import image from "../images/mosaic.svg";
 
 const imageAltText = "purple and blue abstract background";
 
-/**
- * Sort description that expands on your title on the Home component.
- */
-const description =
-  "I'm a MERN stack devloper and I am pursuing my B.Tech degree in CSE studying at Barnett Technical University.";
 
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
+const description =
+  "I am Full Stack + DevOps Developer.I am proficient in CI/CD pipelines, cloud services, and automation, ensuring robust deployment and seamless integration across development";
+
+
 const skillsList = [
-  "Web development",
-  "UX/UI designer",
   "MERN stack",
-  "Data Structures and Algorithms",
+  "DevOps",
+  "Next.js",
+  "AWS",
+  "System Design",
+  "Full Stack Development",
 
 ];
 
-/**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
- */
+
 const detailOrQuote =
-  "I am a MERN stack developer and I am pursuing my B.Tech degree in CSE studying at VIT, Bhopal University. I am a passionate learner and  I love to explore new technologies by which I have command on latest libraries. I am a quick learner and I am always ready to accept new challenges and I try to improve problem solving using data structures and algorithm. I am a hardworking and dedicated person. I am a team player and I am always ready to help my team members. I am a quick learner and I am always ready to accept new challenges. I am a hardworking and dedicated person. I am a team player and I am always ready to help my team members.";
+  "I am a passionate developer proficient in multiple programming languages including C, C++, Python, JavaScript, and TypeScript. My expertise extends to the MERN stack (MongoDB, Express.js, React, Node.js), where I build dynamic and responsive web applications. Additionally, I work with Next.js and Tailwind CSS for efficient front-end development. I have a solid understanding of both SQL and NoSQL databases, particularly PostgreSQL and Prisma, and utilize Node.js frameworks like Hono. My DevOps skills encompass CI/CD pipelines, infrastructure as code, and automation, leveraging cloud technologies like AWS and GCP to deliver scalable and reliable solutions.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,34 +1,10 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
+
 
 import React from "react";
-
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/design-desk.jpeg";
 
 const imageAltText = "desktop with books and laptop";
 
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
     title: "Least Time Travel Path Finder",
@@ -43,9 +19,14 @@ const projectList = [
     url: " github.com/kartikeysharma98281/Marketplace",
   },
   {
-    title: "CBS Flivver",
-    description: "Created a website to felicitate users to search for different motor vehicles and return them with the complete details and price of the vehicle",
-    url: "kartikeysharma98281.github.io/project1.github.io/",
+    title: "Medium Blogging",
+    description: "Pioneered the development of a scalable Medium Blogging platform featuring comprehensive user authentication and sleek UI; utilized Tailwind CSS, Vite, and React.js, boosting user engagement by 40%.",
+    url: "https://medium-blogging-pi.vercel.app/",
+  },
+  {
+    title: "Spotify Clone",
+    description: "Implemented a faithful Spotify Clone project, meticulously designed to mirror the original platform’s interface. The layout and color scheme were optimized to closely resemble Spotify’s design elements, providing users with a familiar and intuitive experience.",
+    url: "https://github.com/kartikeysharma98281/Spotify-clone",
   },
   
 ];
